4af6c439d9100b96fe32cb0a54f2ac6b3860692c
qtanran <quweiyuan@toguide.cn>
2023-06-19 23:08:39 +0800

    feat: 显示SDP信息

--- src/views/localInterflow/index.vue
+++ src/views/localInterflow/index.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 const localVideo = ref('')
 const remoteVideo = ref('')
+const localSdp = ref('')
+const remoteSdp = ref('')
 
 let localStream
 let pc1
@@ -17,11 +19,13 @@ const getLocalDescription = async desc => {
   await pc1.setLocalDescription(desc)
   // 设置远端 SDP 描述信息，即对方发过来的 SDP 数据
   await pc2.setRemoteDescription(desc)
+  localSdp.value = desc.sdp
 }
 
 const getAnswerDescription = async desc => {
   await pc2.setLocalDescription(desc)
   await pc1.setRemoteDescription(desc)
+  remoteSdp.value = desc.sdp
 }
 
 const start = async () => {
@@ -76,13 +80,32 @@ const hangUp = () => {
 </script>
 
 <template>
-  <video ref="localVideo" autoplay playsinline></video>
-  <video ref="remoteVideo" autoplay playsinline></video>
   <div>
     <el-button @click="start">开始</el-button>
     <el-button @click="call">呼叫</el-button>
     <el-button @click="hangUp">挂断</el-button>
   </div>
+  <el-row :gutter="20">
+    <el-col :span="12">
+      <video ref="localVideo" autoplay playsinline></video>
+    </el-col>
+    <el-col :span="12">
+      <video ref="remoteVideo" autoplay playsinline></video>
+    </el-col>
+  </el-row>
+  <el-row :gutter="20">
+    <el-col :span="12">
+      <el-input type="textarea" v-model="localSdp" :rows="15" readonly resize="none" />
+    </el-col>
+    <el-col :span="12">
+      <el-input type="textarea" v-model="remoteSdp" :rows="15" readonly resize="none" />
+    </el-col>
+  </el-row>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+video {
+  background: #222;
+  height: 380px;
+}
+</style>
