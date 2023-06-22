a646c4230c9559ab41614c9e86cd7ca9f7140868
qtanran <quweiyuan@toguide.cn>
2023-06-22 23:24:26 +0800

    feat: 传输速率控制

--- src/views/liveStreaming/index.vue
+++ src/views/liveStreaming/index.vue
@@ -4,7 +4,10 @@ import { io } from 'socket.io-client'
 import { ElMessage } from 'element-plus'
 const localVideo = ref('')
 const remoteVideo = ref('')
-const isShareDesk = ref('false')
+// 是否分享桌面
+const isShareDesk = ref(false)
+// 带宽
+const bandwidth = ref('unlimited')
 const roomId = '111'
 
 let localStream = null
@@ -211,6 +214,30 @@ const leave = () => {
   closeLocalMedia()
   leaveBtnDisabled.value = true
 }
+
+/**
+ * 带宽发生改变
+ */
+const handleBandwidthChange = () => {
+  // 获取到一组 RTCRtpSender 对象，用于接收数据，通常情况下，每一路音视频轨track都会对应有一个sender
+  const senders = pc.getSenders()
+
+  senders.forEach(sender => {
+    if (sender?.track?.kind === 'video') {
+      // 返回一个RTCRtpParameters对象，其中包含有关如何解码RTP数据的信息
+      const parameters = sender.getParameters()
+      if (!parameters.encodings) {
+        parameters.encodings = [{}]
+      }
+      if (bandwidth.value === 'unlimited') {
+        delete parameters.encodings[0].maxBitrate
+      } else {
+        parameters.encodings[0].maxBitrate = bandwidth.value * 1000
+      }
+      sender.setParameters(parameters)
+    }
+  })
+}
 </script>
 
 <template>
@@ -218,6 +245,15 @@ const leave = () => {
     <el-button :disabled="!leaveBtnDisabled" @click="connectServer">连接</el-button>
     <el-button :disabled="leaveBtnDisabled" @click="leave">离开</el-button>
     <el-checkbox v-model="isShareDesk" size="large">共享桌面</el-checkbox>
+    带宽 kb/s
+    <el-select v-model="bandwidth" @change="handleBandwidthChange">
+      <el-option label="无限制" value="unlimited" />
+      <el-option label="2000" value="2000" />
+      <el-option label="1000" value="1000" />
+      <el-option label="500" value="500" />
+      <el-option label="250" value="250" />
+      <el-option label="125" value="125" />
+    </el-select>
   </div>
   <video ref="localVideo" autoplay playsinline muted></video>
   <video ref="remoteVideo" autoplay playsinline></video>
